a0abfa96584780f6fdebbf60e85af5d9c58d63b1
DAVISBH <davisdavi6@gmail.com>
2024-11-25 13:20:35 -0300

    Ordenacao indireta concluida

--- TP1/src/main.c
+++ TP1/src/main.c
@@ -1,25 +1,42 @@
 #include <stdio.h>
 #include "ordind.h"
+#include <sys/time.h>
 
-int main(int argc, char ** argv){
+int main(int argc, char **argv) {
     char aux[100];
     OrdInd_ptr poi = Cria();
-    CarregaArquivo(poi,"cad/entrada2.xcsv");
+    
+    CarregaArquivo(poi, "cad/entrada3.xcsv");
 
     int numatrib = NumAtributos(poi);
-    
-    for (int i = 0; i < numatrib; i++){
-        if (NomeAtributo(poi,i,aux)){
-            if (!strcmp(aux,"name")||!strcmp(aux,"id")||!strcmp(aux,"address")){
-                CriaIndice(poi,i);
-                OrdenaSelectionSort(poi, i);     
-                OrdenaBubbleSort(poi, i);
-                OrdenaQuickSort(poi, i, 0, poi->numRegistros - 1);
-                ImprimeOrdenadoIndice (poi, i);
-                getchar();
-            } 
+    char *opcao = argv[1];
+
+    struct timeval start, end;
+    gettimeofday(&start, NULL);
+
+    for (int i = 0; i < numatrib; i++) {
+        if (NomeAtributo(poi, i, aux)) {
+            if (!strcmp(aux, "name") || !strcmp(aux, "id") || !strcmp(aux, "address")) {
+                CriaIndice(poi, i);
+                switch (*opcao) {
+                    case 'q':
+                        OrdenaQuickSort(poi, i, 0, poi->numRegistros - 1);
+                        break; // Adiciona o break
+                    case 's':
+                        OrdenaSelectionSort(poi, i);
+                        break; // Adiciona o break
+                    case 'b': 
+                        OrdenaBubbleSort(poi, i);
+                        break; // Adiciona o break
+                    // ImprimeOrdenadoIndice(poi, i);
+                }
+            }
         }
     }
+
+    gettimeofday(&end, NULL);
+    double time_taken = (end.tv_sec - start.tv_sec) + (end.tv_usec - start.tv_usec) / 1000000.0;
+    printf("Tempo de execução: %f segundos\n", time_taken);
 
     Destroi(poi);
     return 0;
